a0d4385d3cdf92e308b5cc058d92f223f44dc63d
Micah Johnson <mjohnson@teleflora.org>
2020-06-11 12:09:36 -0500

    init upload

---
+++ src/app/elements/elements.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ElementsRoutingModule } from './elements-routing.module';
+import { ElementsHomeComponent } from './elements-home/elements-home.component';
+import { PlaceHolderComponent } from './place-holder/place-holder.component';
+import { TimesDirective } from './times.directive';
+
+
+@NgModule({
+  declarations: [ElementsHomeComponent, PlaceHolderComponent, TimesDirective],
+  imports: [
+    CommonModule,
+    ElementsRoutingModule
+  ],
+  exports: []
+})
+export class ElementsModule { }
