2a2f623048bbfd4f3f6e56f6c9a70685f779f21a
mjohnson <mjohnson@OKC-L-8CRZTF2>
2021-03-02 14:29:35 -0600

    Changes before update to angular

--- src/app/elements/elements.module.ts
+++ src/app/elements/elements.module.ts
@@ -5,13 +5,15 @@ import { ElementsRoutingModule } from './elements-routing.module';
 import { ElementsHomeComponent } from './elements-home/elements-home.component';
 import { PlaceHolderComponent } from './place-holder/place-holder.component';
 import { TimesDirective } from './times.directive';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
   declarations: [ElementsHomeComponent, PlaceHolderComponent, TimesDirective],
   imports: [
     CommonModule,
-    ElementsRoutingModule
+    ElementsRoutingModule,
+    SharedModule
   ],
   exports: []
 })
